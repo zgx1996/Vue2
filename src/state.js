1c9e0280a60853ef2c44e2319ea9c2b6f1103def
funlab <2458734304@qq.com>
2022-07-06 08:21:59 +0800

    computed 01

--- src/state.js
+++ src/state.js
@@ -1,7 +1,12 @@
-import { observe } from './observe/index.js'
+import {
+  observe
+} from './observe/index.js'
 export function initState(vm) {
   if (vm.$options.data) {
     initData(vm)
+  }
+  if (vm.$options.computed) {
+    initComputed(vm)
   }
 }
 
@@ -11,6 +16,24 @@ function initData(vm) {
   vm._data = data
   observe(data)
   proxy(vm, '_data', data)
+}
+
+function initComputed(vm) {
+  let computed = vm.$options.computed
+  if (computed) {
+    for (let key in computed) {
+      defineComputed(vm, key, computed[key])
+    }
+  }
+}
+
+function defineComputed(target, key, userDef) {
+  const getter = typeof userDef === 'function' ? userDef : userDef.get
+  const setter = userDef.set || (() => {})
+  Object.defineProperty(target, key, {
+    get: getter,
+    set: setter
+  })
 }
 
 function proxy(vm, target, data) {
